80498396fd79c2dd834b4d22c12336b9cee66650
PTeng <pompyt@yahoo.co.uk>
2018-08-10 16:34:56 +0100

    Initial commit

--- tests/features/pages/basePage.js
+++ tests/features/pages/basePage.js
@@ -4,41 +4,41 @@ const using = webdriver.By;
 
 class BasePage {
 
-    constructor(driver, cssSelector = '') {
+    constructor(driver, selector = '') {
         if (!driver) {
             throw new Error('driver not specified');
         }
 
         this.driver = driver;
-        this.cssSelector = cssSelector;
+        this.selector = selector;
     }
 
     async findElement() {
-        const elements = await this.driver.findElements(using.css(this.cssSelector));
+        const elements = await this.driver.findElements(using.css(this.selector));
         return elements[0];
     }
 
-    async findElementByCss(cssSelector) {
-        const elements = await this.driver.findElements(using.css(cssSelector));
+    async findElementByCss(selector) {
+        const elements = await this.driver.findElements(using.css(selector));
         return elements[0];
     }
 
-    async findElementsByCss(cssSelector) {
-        return await this.driver.findElements(using.css(cssSelector));
+    async findElementsByCss(selector) {
+        return await this.driver.findElements(using.css(selector));
     }
 
-    async findChildByCss(childCssSelector) {
-        const children = await this.findChildrenByCss(childCssSelector);
+    async findChildByCss(childselector) {
+        const children = await this.findChildrenByCss(childselector);
         return children[0];
     }
 
-    async findChildrenByCss(childCssSelector) {
-        const fullSelector = `${this.cssSelector} ${childCssSelector}`;
+    async findChildrenByCss(childSelector) {
+        const fullSelector = `${this.selector} ${childSelector}`;
         return await this.driver.findElements(using.css(fullSelector));
     }
 
     async exists() {
-        const elements = await this.driver.findElements(using.css(this.cssSelector));
+        const elements = await this.driver.findElements(using.css(this.selector));
         return elements.length > 0;
     }
 
@@ -51,7 +51,7 @@ class BasePage {
     }
 
     async waitUntilExists(timeout) {
-        const waitCondition = until.elementLocated(using.css(this.cssSelector));
+        const waitCondition = until.elementLocated(using.css(this.selector));
         await this.driver.wait(waitCondition, timeout);
     }
 }
