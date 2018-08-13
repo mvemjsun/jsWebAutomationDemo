0ce7621ffffabbf19850d939c9552884b819daf5
PTeng <pompyt@yahoo.co.uk>
2018-08-13 12:57:10 +0100

    Initial commit

--- src/login.js
+++ src/login.js
@@ -7,6 +7,12 @@ class LoginHelper {
       this.pw = credentials.pw;
     }
 
+    /*
+    * This is a mock function that allows a successful resolution only when
+    * the username and password match to the coded values. This is for demonstration
+    * only. In real life this would be validated against a stored values possibly
+    * in an encrypted database table.
+    */
     validateCredentials() {
         return new Promise((resolve, reject) => {
           if (this.un.toUpperCase() === 'ME@EMAIL.COM' && this.pw === 'password') {
