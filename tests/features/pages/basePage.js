d6c1fc309acf1c25a31ed0ed6fc4fe723e93a061
PTeng <pompyt@yahoo.co.uk>
2018-08-13 20:10:24 +0100

    Initial commit

--- tests/features/pages/basePage.js
+++ tests/features/pages/basePage.js
@@ -5,10 +5,6 @@ const using = webdriver.By;
 class BasePage {
 
     constructor(driver, selector = '') {
-        if (!driver) {
-            throw new Error('driver not specified');
-        }
-
         this.driver = driver;
         this.selector = selector;
     }
@@ -23,36 +19,9 @@ class BasePage {
         return elements[0];
     }
 
-    async findElementsByCss(selector) {
-        return await this.driver.findElements(using.css(selector));
-    }
-
-    async findChildByCss(childselector) {
-        const children = await this.findChildrenByCss(childselector);
-        return children[0];
-    }
-
-    async findChildrenByCss(childSelector) {
-        const fullSelector = `${this.selector} ${childSelector}`;
-        return await this.driver.findElements(using.css(fullSelector));
-    }
-
     async exists() {
         const elements = await this.driver.findElements(using.css(this.selector));
         return elements.length > 0;
-    }
-
-    async isVisible() {
-        const element = await this.findElement();
-        if (!element) {
-            return false;
-        }
-        return await element.isDisplayed();
-    }
-
-    async waitUntilExists(timeout) {
-        const waitCondition = until.elementLocated(using.css(this.selector));
-        await this.driver.wait(waitCondition, timeout);
     }
 }
 
